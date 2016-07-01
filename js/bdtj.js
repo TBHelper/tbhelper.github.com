dac7327699eff4bdcf9bb9b067657edac0a761fc
TBHelper <1815570077@qq.com>
2016-07-01 14:14:51 +0800

    first commit

---
+++ js/bdtj.js
@@ -0,0 +1,10 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?54a712fd5b8b909f9af8dd69afb96329";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
